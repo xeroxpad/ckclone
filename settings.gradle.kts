90240d75e47dfb8b2055ff0fb94a35cbd4fed0bc
mikhail_nikonov <xerox.pad@mail.ru>
2024-01-19 10:08:25 +0300

    webview vk

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,28 @@
+import java.net.URI
+
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = URI("https://artifactory-external.vkpartner.ru/artifactory/vkid-sdk-andorid/")
+        }
+        maven {
+            url = URI("https://artifactory-external.vkpartner.ru/artifactory/superappkit-maven-public/")
+        }
     }
 }
+
+
 
 rootProject.name = "VkClone"
 include(":app")
